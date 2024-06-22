b147e546f27e65a664f7b27c2e7bb2370b35e983
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-22 17:11:19 +0530

    exercise 3.7 part 7

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+var morgan = require("morgan")
 
-app.use(express.json());
+const app = express()
+
+app.use(express.json())
+app.use(morgan('tiny'))
 
 var persons = [
     {
