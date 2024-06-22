06739cc7fda1992c4cac66e42a24793885ae4bca
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-22 17:18:36 +0530

    exercise 3.8 step 8

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var morgan = require("morgan")
 
 const app = express()
 
+morgan.token('body', function (req, res) { if (req.method === 'POST') { return JSON.stringify(req.body) } })
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 var persons = [
     {
@@ -52,7 +54,7 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number)  {
+    if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number missing'
         })
