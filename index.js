6a9bbedc1096cb589ae6c390de69a17a32252d56
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-07-05 21:36:15 +0530

    3.11 fullstack phonebook

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 
 morgan.token('body', function (req, res) { if (req.method === 'POST') { return JSON.stringify(req.body) } })
 
+app.use(express.static('dist'))
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
